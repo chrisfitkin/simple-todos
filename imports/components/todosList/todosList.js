df3ef04c4278e7e9c9563affd5bd8d6fbade0334
chrisfitkin <cfitkin@dclan.com>
2016-09-07 11:45:00 -0700

    7 filtering and collections

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -8,13 +8,31 @@ class TodosListCtrl {
   constructor($scope) {
     $scope.viewModel(this);
 
+    this.hideCompleted = false;
+
     this.helpers({
       tasks() {
-        return Tasks.find({}, {
+        const selector = {};
+
+        // If hide completed is checked, filter tasks
+        if (this.getReactively('hideCompleted')) {
+          selector.checked = {
+            $ne: true
+          };
+        }
+
+        return Tasks.find(selector, {
           sort: {
             createdAt: -1
           }
         });
+      },
+      incompleteCount() {
+        return Tasks.find({
+          checked: {
+            $ne: true
+          }
+        }).count();
       }
     })
   }
@@ -28,6 +46,19 @@ class TodosListCtrl {
 
     // Clear form
     this.newTask = '';
+  }
+
+  setChecked(task) {
+    // Set the checked property to the opposite of its current value
+    Tasks.update(task._id, {
+      $set: {
+        checked: !task.checked
+      },
+    });
+  }
+
+  removeTask(task) {
+    Tasks.remove(task._id);
   }
 }
 export default angular.module('todosList', [
--- readme.txt
+++ readme.txt
@@ -2,3 +2,5 @@
 https://www.meteor.com/tutorials/angular/creating-an-app
 
 https://www.meteor.com/tutorials/angular/templates
+
+https://www.meteor.com/tutorials/angular/adding-user-accounts
